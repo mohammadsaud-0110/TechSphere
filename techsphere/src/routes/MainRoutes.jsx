7577ffba756de1a5060e0c7003c8da6a045d2761
Hemandh <hemandh007bond@gmail.com>
2023-06-24 12:33:47 +0530

    quiz section

--- techsphere/src/routes/MainRoutes.jsx
+++ techsphere/src/routes/MainRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../components/login";
 import { HomePage } from "../components/home";
 import { Register } from "../components/register";
+import Quiz from "../components/quiz/Quiz";
 
 export const MainRoutes = () => {
   return (
@@ -10,6 +11,7 @@ export const MainRoutes = () => {
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/quiz" element={<Quiz />} />
     </Routes>
   );
 };
